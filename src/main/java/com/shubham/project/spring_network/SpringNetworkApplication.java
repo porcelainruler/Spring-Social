8303a100a217544e3f7b4e32420302014337280c
Shubham <k.shubham.22699@gmail.com>
2023-08-27 14:24:44 +0530

    Shubham | PROJECT-INIT | UML Class diagram basic finished with uml use case diagram file checkin | TBD -> Complete use case diagram

---
+++ src/main/java/com/shubham/project/spring_network/SpringNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.shubham.project.spring_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubham/project/spring_network/SpringNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubham.project.spring_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
