850e54c7acd4476883e09dddea83dc4f19b0a957
Shubham <k.shubham.22699@gmail.com>
2023-11-27 20:46:16 +0530

    Shubham | Logger cofiguration, color coding, rotation policy added

--- src/main/java/com/shubham/project/spring_network/controller/Test.java
+++ src/main/java/com/shubham/project/spring_network/controller/Test.java
@@ -1,14 +1,28 @@
 package com.shubham.project.spring_network.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Test Group", description = "A group of testing apis")
 @RestController
+@Log4j2
 public class Test {
     @GetMapping("/test")
     public String getHello () {
         return "Hello There !!!";
+    }
+
+    @GetMapping("/lombok")
+    public String index() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
     }
 }
