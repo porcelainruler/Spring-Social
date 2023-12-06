7d5758959262667133a6000f1a63742cde98738e
shubham <k.shubham.22699@gmail.com>
2023-12-06 19:47:41 +0530

    Shubham | Auth sucess and failure event handler classes checkin with empty body / logic

---
+++ src/main/java/com/shubham/project/spring_network/events/LoginFailureEventHandler.java
@@ -0,0 +1,16 @@
+package com.shubham.project.spring_network.events;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class LoginFailureEventHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+    }
+}
---
+++ src/main/java/com/shubham/project/spring_network/events/LoginSuccessEventHandler.java
@@ -0,0 +1,18 @@
+package com.shubham.project.spring_network.events;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class LoginSuccessEventHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+
+    }
+}
